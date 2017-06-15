2531b38126f7380774360e3eabcb23ddb839f1c1
TimRChen <timrchen@foxmail.com>
2017-06-15 18:42:35 +0800

    update chat-room file

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const app = require('express')();
-
-
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html', function () {
-      console.log('the page is open.')
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
 });
 
-
-app.listen(3000, () => {
-    console.log('listening on port@3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
